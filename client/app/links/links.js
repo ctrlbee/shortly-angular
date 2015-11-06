0df51d31d56197611c8999022ca5d38c97b0206f
ctrlbee <chris@ctrlbee.com>
2015-11-06 15:38:03 -0800

    Adding LinkController and template to page

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,20 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.links = [
+    {
+      visits: 5,
+      title: 'fhdjsjh',
+      url: 'hfjkadhfjdk',
+      base_url: 'fjhdah',
+      code: 'fjdskj'
+    },
+    {
+      visits: 4,
+      title: 'jsjh',
+      url: 'jk',
+      base_url: 'dah',
+      code: 'dskj'
+    }
+  ];
 });
