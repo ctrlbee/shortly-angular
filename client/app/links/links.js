3216190ec89c359190ed407c00c7cd96d4c9cdd7
ctrlbee <chris@ctrlbee.com>
2015-11-06 16:05:04 -0800

    LinkController and Links factory working

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,20 +1,28 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  $scope.links = [
-    {
-      visits: 5,
-      title: 'fhdjsjh',
-      url: 'hfjkadhfjdk',
-      base_url: 'fjhdah',
-      code: 'fjdskj'
-    },
-    {
-      visits: 4,
-      title: 'jsjh',
-      url: 'jk',
-      base_url: 'dah',
-      code: 'dskj'
-    }
-  ];
+  $scope.links = [];
+
+  $scope.getLinks = function() {
+    Links.getLinks().then(function(links) {
+      $scope.links = links;
+    });
+  } 
+
+  // $scope.links = [
+  //   {
+  //     visits: 5,
+  //     title: 'fhdjsjh',
+  //     url: 'hfjkadhfjdk',
+  //     base_url: 'fjhdah',
+  //     code: 'fjdskj'
+  //   },
+  //   {
+  //     visits: 4,
+  //     title: 'jsjh',
+  //     url: 'jk',
+  //     base_url: 'dah',
+  //     code: 'dskj'
+  //   }
+  // ];
 });
